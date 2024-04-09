a4bc95aaf8d6a3914ef568210268508d9e3d32f9
Tordnado <tord.saetermo@gmail.com>
2024-04-09 16:40:11 +0200

    feet: part four and five

--- exercise-5/messagepassing/request.go
+++ exercise-5/messagepassing/request.go
@@ -1,12 +1,12 @@
 package main
 
-import "fmt"
-import "time"
-import "sort"
-
+import (
+	"fmt"
+	"sort"
+	"time"
+)
 
 const tick = time.Millisecond * 33
-
 
 // --- RESOURCE ROUTINE --- //
 /*
@@ -27,116 +27,126 @@ Hints:
         other stuff...
     }
     ```
- - Use the priority queue (scroll down below `main()`. And remember the type-assertion thing:  
+ - Use the priority queue (scroll down below `main()`. And remember the type-assertion thing:
     ```Go
     request := queue.Front().(ResourceRequest)
     ```
 */
 
 type Resource struct {
-    value []int // Resource type is []int. Each user appends its own id when executing.
+	value []int // Resource type is []int. Each user appends its own id when executing.
 }
 
 type ResourceRequest struct {
-    id          int
-    priority    int
-    channel     chan Resource
+	id       int
+	priority int
+	channel  chan Resource
 }
 
-func resourceManager(askFor chan ResourceRequest, giveBack chan Resource){
+func resourceManager(askFor chan ResourceRequest, giveBack chan Resource) {
 
-    res     := Resource{}
-    //busy    := false
-    //queue   := PriorityQueue{}
+	res := Resource{}
+	busy := false
+	queue := PriorityQueue{}
 
-    for {
-        select {
-        case request := <-askFor:
-            //fmt.Printf("[resource manager]: received request: %+v\n", request)
-            request.channel <- res
-        case res = <-giveBack:
-            //fmt.Printf("[resource manager]: resource returned\n")
-        }
-    }
+	for {
+		select {
+		case request := <-askFor:
+			//fmt.Printf("[resource manager]: received request: %+v\n", request)
+			queue.Insert(request.channel, request.priority)
+
+		case res = <-giveBack:
+			//fmt.Printf("[resource manager]: resource returned\n")
+			queue.PopFront()
+			busy = false
+
+		}
+
+		if !busy && !queue.Empty() {
+			busy = true
+
+			reschan, ok := queue.Front().(chan Resource)
+
+			if ok {
+				reschan <- res
+			}
+		}
+	}
 }
-
 
 // --- RESOURCE USERS -- //
 
 type ResourceUserConfig struct {
-    id          int
-    priority    int
-    release     int
-    execution   int
+	id        int
+	priority  int
+	release   int
+	execution int
 }
 
-func resourceUser(cfg ResourceUserConfig, askFor chan ResourceRequest, giveBack chan Resource){
+func resourceUser(cfg ResourceUserConfig, askFor chan ResourceRequest, giveBack chan Resource) {
 
-    replyChan := make(chan Resource)
+	replyChan := make(chan Resource)
 
-    time.Sleep(time.Duration(cfg.release) * tick)
+	time.Sleep(time.Duration(cfg.release) * tick)
 
-    executionStates[cfg.id] = waiting
-    askFor <- ResourceRequest{cfg.id, cfg.priority, replyChan}
-    res := <- replyChan
+	executionStates[cfg.id] = waiting
+	askFor <- ResourceRequest{cfg.id, cfg.priority, replyChan}
+	res := <-replyChan
 
-    executionStates[cfg.id] = executing
+	executionStates[cfg.id] = executing
 
-    time.Sleep(time.Duration(cfg.execution) * tick)
-    res.value = append(res.value, cfg.id)
-    giveBack <- res
+	time.Sleep(time.Duration(cfg.execution) * tick)
+	res.value = append(res.value, cfg.id)
+	giveBack <- res
 
-    executionStates[cfg.id] = done
+	executionStates[cfg.id] = done
 }
 
+func main() {
+	askFor := make(chan ResourceRequest, 10)
+	giveBack := make(chan Resource)
+	go resourceManager(askFor, giveBack)
 
-func main(){
-    askFor      := make(chan ResourceRequest, 10)
-    giveBack    := make(chan Resource)
-    go resourceManager(askFor, giveBack)
+	executionStates = make([]ExecutionState, 10)
 
-    executionStates = make([]ExecutionState, 10)
+	cfgs := []ResourceUserConfig{
+		{0, 0, 1, 1},
+		{1, 0, 3, 1},
+		{2, 1, 5, 1},
 
-    cfgs := []ResourceUserConfig{
-        {0, 0, 1, 1},
-        {1, 0, 3, 1},
-        {2, 1, 5, 1},
+		{0, 1, 10, 2},
+		{1, 0, 11, 1},
+		{2, 1, 11, 1},
+		{3, 0, 11, 1},
+		{4, 1, 11, 1},
+		{5, 0, 11, 1},
+		{6, 1, 11, 1},
+		{7, 0, 11, 1},
+		{8, 1, 11, 1},
 
-        {0, 1, 10, 2},
-        {1, 0, 11, 1},
-        {2, 1, 11, 1},
-        {3, 0, 11, 1},
-        {4, 1, 11, 1},
-        {5, 0, 11, 1},
-        {6, 1, 11, 1},
-        {7, 0, 11, 1},
-        {8, 1, 11, 1},
+		{0, 1, 25, 3},
+		{6, 0, 26, 2},
+		{7, 0, 26, 2},
+		{1, 1, 26, 2},
+		{2, 1, 27, 2},
+		{3, 1, 28, 2},
+		{4, 1, 29, 2},
+		{5, 1, 30, 2},
+	}
 
-        {0, 1, 25, 3},
-        {6, 0, 26, 2},
-        {7, 0, 26, 2},
-        {1, 1, 26, 2},
-        {2, 1, 27, 2},
-        {3, 1, 28, 2},
-        {4, 1, 29, 2},
-        {5, 1, 30, 2},
-    }
+	go executionLogger()
+	for _, cfg := range cfgs {
+		go resourceUser(cfg, askFor, giveBack)
+	}
 
-    go executionLogger()
-    for _, cfg := range cfgs {
-        go resourceUser(cfg, askFor, giveBack)
-    }
+	// (no way to join goroutines, hacking it with sleep)
+	time.Sleep(time.Duration(45) * tick)
 
-    // (no way to join goroutines, hacking it with sleep)
-    time.Sleep(time.Duration(45) * tick)
-
-    resourceCh := make(chan Resource)
-    askFor <- ResourceRequest{0, 1, resourceCh}
-    executionOrder := <-resourceCh
-    fmt.Println("Execution order:", executionOrder)
+	resourceCh := make(chan Resource)
+	askFor <- ResourceRequest{0, 1, resourceCh}
+	executionOrder := <-resourceCh
+	fmt.Println("Execution order:", executionOrder)
 }
-
-
 
 // --- PRIORITY QUEUE --- //
 /*
@@ -149,66 +159,68 @@ You will have to use a "type assertion" to cast the interface{} to the correct t
 `first := queue.Front().(YourCustomType)`
 */
 type PriorityQueue struct {
-    queue []struct {
-        val         interface{}
-        priority    int
-    }
+	queue []struct {
+		val      interface{}
+		priority int
+	}
 }
-func (pq *PriorityQueue) Insert(value interface{}, priority int){
-    pq.queue = append(pq.queue, struct{val interface{}; priority int}{value, priority})
-    sort.SliceStable(pq.queue, func(i, j int) bool {
-        return pq.queue[i].priority > pq.queue[j].priority
-    })
+
+func (pq *PriorityQueue) Insert(value interface{}, priority int) {
+	pq.queue = append(pq.queue, struct {
+		val      interface{}
+		priority int
+	}{value, priority})
+	sort.SliceStable(pq.queue, func(i, j int) bool {
+		return pq.queue[i].priority > pq.queue[j].priority
+	})
 }
 func (pq *PriorityQueue) Front() interface{} {
-    return pq.queue[0].val
+	return pq.queue[0].val
 }
-func (pq *PriorityQueue) PopFront(){
-    pq.queue = pq.queue[1:]
+func (pq *PriorityQueue) PopFront() {
+	pq.queue = pq.queue[1:]
 }
-func(pq *PriorityQueue) Empty() bool {
-    return len(pq.queue) == 0
+func (pq *PriorityQueue) Empty() bool {
+	return len(pq.queue) == 0
 }
-
-
 
 // --- EXECUTION LOGGING --- //
 
 type ExecutionState rune
+
 const (
-    none    ExecutionState  = '\u0020'
-    waiting                 = '\u2592'
-    executing               = '\u2593'
-    done                    = '\u2580'
+	none      ExecutionState = '\u0020'
+	waiting                  = '\u2592'
+	executing                = '\u2593'
+	done                     = '\u2580'
 )
 
 var executionStates []ExecutionState
 
-func executionLogger(){
-    time.Sleep(tick/2)
-    t := 0
+func executionLogger() {
+	time.Sleep(tick / 2)
+	t := 0
 
+	fmt.Printf("  id:")
+	for id, _ := range executionStates {
+		fmt.Printf("%3d", id)
+	}
+	fmt.Printf("\n")
 
-    fmt.Printf("  id:")
-    for id, _ := range executionStates {
-        fmt.Printf("%3d", id)
-    }
-    fmt.Printf("\n")
-
-    for {
-        grid := ' ';
-        if t % 5 == 0 {
-            grid = '\u2500'
-        }
-        fmt.Printf("%04d : ", t)
-        for id, state := range executionStates {
-            fmt.Printf("%c%c%c", state, grid, grid)
-            if state == done {
-                executionStates[id] = none
-            }
-        }
-        fmt.Printf("\n")
-        t++
-        time.Sleep(tick)
-    }
+	for {
+		grid := ' '
+		if t%5 == 0 {
+			grid = '\u2500'
+		}
+		fmt.Printf("%04d : ", t)
+		for id, state := range executionStates {
+			fmt.Printf("%c%c%c", state, grid, grid)
+			if state == done {
+				executionStates[id] = none
+			}
+		}
+		fmt.Printf("\n")
+		t++
+		time.Sleep(tick)
+	}
 }
